78516130f309c1176d112288b9ad547a94ba723c
aizazulllhaq <aizazulllhaq@gmail.com>
2024-06-14 00:21:06 -0700

    fix - Frontend URL

--- backend-ums/src/constant.js
+++ backend-ums/src/constant.js
@@ -22,3 +22,5 @@ export const CORS_ORIGIN = process.env.CORS_ORIGIN;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+
+export const FRONTEND_URL = process.env.FRONTEND_URL;
